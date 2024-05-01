3728dea869814cc31c9b4dcd431f2b9f71dd9818
Zain Ul Abideen Alvi <zainalvi552@gmail.com>
2024-05-01 16:29:22 +0500

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,16 @@
+
+  //
+  const marvel=["iron man","batman","superman"]
+  const f4=["zain","alvi"]
+  const allarr=[...marvel ,...f4]
+  console.log(allarr)
+
+
+  let arr=[1,2,3,4,[5,[6,],7],[8,9,],10]
+  const arre=arr.flat(Infinity)
+  console.log(arre)
+  console.log(Array.from("zainalvi"))
+  let run1=100
+  let run2=200
+  let run3=300
+  console.log(Array.of(run1,run2,run3))
